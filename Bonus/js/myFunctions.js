90e13395584af40f5373d8dc8d744b2c3893bd34
Salvatore Capano <capano_salvatore@libero.it>
2023-01-13 09:47:16 +0100

    Completato esercizio bonus

---
+++ Bonus/js/myFunctions.js
@@ -0,0 +1,114 @@
+// Imposta dimensione celle in base al loro numero
+function setCellSize (number) {
+
+    if (number == 100) {
+        root.style.setProperty("--cellSize", "calc(100% / 10)");
+    }
+    else if (number == 81) {
+        root.style.setProperty("--cellSize", "calc(100% / 9)");
+    }
+    else if (number == 49) {
+        root.style.setProperty("--cellSize", "calc(100% / 7)");
+    }
+};
+
+// Genera un numero casuale tra 1 e index
+function getRandomNumber(index) {
+    return Math.floor(Math.random() * (index - 1) + 1)
+}
+
+// Genera l'array contenente le posizioni delle bombe
+function setBombsPositions (maxIndex) {
+
+    let myArray = [];
+    let newRandomNumber;
+
+    for (let i = 0; i < 16; i++) {
+
+        do {
+            newRandomNumber = getRandomNumber (maxIndex);
+        }
+        while (myArray.includes(newRandomNumber));
+
+        myArray.push(newRandomNumber);
+    }
+
+    return myArray;
+};
+
+// Controlla se l'elemento cliccato e' una bomba
+function checkGameOver (array, currentIndex) {
+
+    // Controlla se la cella corrisponde ad una bomba
+    if (array.includes(parseInt(currentIndex))) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// Mostra tutte le bombe
+function showBombs (bombsPositions) {
+    let allCells = document.querySelectorAll(".cell");
+
+    for (let i = 0; i < 16; i++) {
+        allCells[bombsPositions[i]].classList.add("bomb");
+        allCells[bombsPositions[i]].classList.add("active");
+        console.log("Sto mostrando le bombe...");;
+    }
+}
+
+// Crea un elemento cella e applica l'event Listener
+function createCell (index) {
+
+    // Crea la cella e ci stampa il numero corrispondente
+    const newElement = document.createElement("div");
+    newElement.classList.add("cell");
+    newElement.innerText = index + 1;
+
+    // Listener Evento click sulla cella 
+    newElement.addEventListener("click", function(){
+        console.log("----------");
+        console.log("Hai cliccato la cella", this.innerText);
+
+        // Caso partita persa
+        if (checkGameOver(bombsPositions, this.innerText)) {
+            this.classList.add("bomb");
+            console.log("BOMBA");
+            
+            showBombs(bombsPositions);
+            grid.classList.add("overlay");
+            root.style.setProperty("--overlayMessage", `'game over'`);
+        } 
+        // Caso partita non persa
+        else {
+            if (!(this.classList.contains("active"))) {
+                score++;
+                scoreContainer.innerText = `Il tuo punteggio è ${score}`;
+                // Caso partita vinta
+                if (score >= maxScore) {
+                    showBombs(bombsPositions);
+                    grid.classList.add("overlay");
+                    root.style.setProperty("--overlayMessage", `'hai vinto'`);
+                }
+            }
+        }
+        this.classList.add("active");
+        console.log("Punteggio:", score);
+    });
+
+    return newElement;
+};
+
+// Riempie la griglia con gli elementi cella creati
+function fillGrid (maxIndex) {
+
+    for (let i = 0; i < maxIndex; i++) {
+
+        // Creazione e aggiunta nuovo elemento cell
+        newCell = createCell (i);
+        grid.append(newCell);
+    };
+
+};
---
+++ Bonus/js/script.js
@@ -0,0 +1,73 @@
+/*
+
+-Generare 16 numeri casuali diversi tra loro:
+    -Range 1-100
+    -Range 1-81
+    -Range 1-49
+-Al click sulla cella Controllare uguaglianza numero cliccato e elementi array bomba
+    -Se Uguali: 
+        -applicare alla cella colore sconfitta
+        -partita terminata
+        -stampare punteggio
+        -rendere le celle non cliccabili
+        -Mostrare tutte le bombe
+    -Se Non uguali:
+        -applicare alla cella colore vittoria
+        -aumentare punteggio
+        -controllare punteggio:
+            -se punteggio >= (numero celle - numero bombe)
+                -partita terminata
+                -stampare punteggio
+                -rendere le celle non cliccabili
+                -Mostrare tutte le bombe
+
+*/
+
+const root = document.querySelector(":root");
+const playBtn = document.querySelector("#playBtn");
+const grid = document.querySelector(".grid");
+const scoreContainer = document.querySelector(".scoreContainer");
+
+const difficulty = document.querySelector("#difficulty");
+
+let cellsNumber;
+let bombsPositions;
+let score;
+
+// Nuova partita
+playBtn.addEventListener("click", function(){
+
+    // Svuota la griglia
+    grid.innerHTML = "";
+    grid.classList.remove("overlay");
+    root.style.setProperty("--overlayMessage", `''`);
+
+    // Svuota la console
+    console.clear();
+
+    // Resetta punteggio
+    score = 0;
+    scoreContainer.innerText = `Il tuo punteggio è ${score}`;
+    
+    // Se non presenti, mostra la griglia e il container del punteggio
+    if (!grid.classList.contains("show") || !scoreContainer.classList.contains("show")) {
+        grid.classList.add("show");
+        scoreContainer.classList.add("show");
+    }
+
+    // Ottiene il numero di celle da generare e le dimensiona di conseguenza
+    cellsNumber = difficulty.value;
+    setCellSize(cellsNumber);
+
+    // Generazione posizioni bombe
+    bombsPositions = setBombsPositions(cellsNumber).sort((a,b)=>a-b);
+    console.log(`Ci sono ${bombsPositions.length} bombe`);
+    console.log("Bombe generate alle posizioni:", bombsPositions);
+
+    // Calcolo Punteggio massimo raggiungibile
+    maxScore = cellsNumber - bombsPositions.length;
+    console.log(`Obbiettivo: arrivare a ${maxScore} punti`);
+
+    // Riempie la griglia
+    fillGrid (cellsNumber);
+});
